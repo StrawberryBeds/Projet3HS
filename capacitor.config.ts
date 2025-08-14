005476471789e6bdb152510fb1f6e6e6d74e42c4
StrawberryBeds <samuel.l.wood@gmail.com>
2025-08-14 11:58:25 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Projet3HS',
+  webDir: 'dist'
+};
+
+export default config;
