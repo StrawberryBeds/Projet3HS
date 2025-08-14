da5a0835e87ec9739124bd66a926f5ddfde27520
StrawberryBeds <samuel.l.wood@gmail.com>
2025-08-14 12:12:54 -0400

    Adds firebaseConfig.js

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxEf1yI7NY3oR_nZqt303O3QMlyiPocVo",
+  authDomain: "projet3hs.firebaseapp.com",
+  projectId: "projet3hs",
+  storageBucket: "projet3hs.firebasestorage.app",
+  messagingSenderId: "716151265395",
+  appId: "1:716151265395:web:1afda5c3b0430531d7f206"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
